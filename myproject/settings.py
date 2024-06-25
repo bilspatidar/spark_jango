4311418363ec3bd920f22fcdeeffbfc589be0247
nirbhay dhangar <nirbhaydhangar@gmail.com>
2024-06-25 15:52:37 +0530

    mail send complete

--- myproject/settings.py
+++ myproject/settings.py
@@ -90,6 +90,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'emails/templates'),
+            os.path.join(BASE_DIR, 'emails', 'templates'),  
         ],
         'APP_DIRS': True,
         'OPTIONS': {
